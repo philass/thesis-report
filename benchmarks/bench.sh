1c265307fd4cbd19db15d39c4943798ef71a62d5
Philip Lassen <philiplassen+git@gmail.com>
2021-06-10 17:07:16 -0700

    Helpful bash scripts for compiling and benchmarking

--- benchmarks/bench.sh
+++ benchmarks/bench.sh
@@ -1,3 +1,7 @@
+./compile.sh
+RUNS=$2
+echo "c"
+echo $1 | ./prefix_sum_seq -t /dev/stdout
 echo "c"
 echo $1 | ./prefix_sum_seq -t /dev/stdout
 for i in 0 1 2 4 8 12 16
@@ -10,5 +14,5 @@ echo $1 | node ./prefix_sum_wasm_seq -t /dev/stdout
 for i in 0 1 2 4 8 12 16
 do
   echo "wasm-multicore" $i
-  echo $1 | node --experimental-wasm-threads ./prefix_sum_wasm_mc -t /dev/stdout --num-threads=$i
+  echo $1 | timeout 1 node --experimental-wasm-threads ./prefix_sum_wasm_mc -t /dev/stdout --num-threads=$i
 done
--- benchmarks/compile.sh
+++ benchmarks/compile.sh
@@ -1,8 +1,11 @@
 echo "compiling wasm-multicore"
-EMCFLAGS="-s INITIAL_MEMORY=201326592 -s PTHREAD_POOL_SIZE=12" futhark wasm-multicore prefix_sum.fut -o prefix_sum_wasm_mc
+# Initial memory of 201326592 is 192 megabytes
+MEMORY=$((2147483648-64*2**10)) # 2gb approx
+
+EMCFLAGS="-s INITIAL_MEMORY=$MEMORY -s PTHREAD_POOL_SIZE=12" futhark wasm-multicore prefix_sum.fut -o prefix_sum_wasm_mc
 echo "compiling wasm"
 # Setting additional memory is optional for sequential wasm
-EMCFLAGS="-s INITIAL_MEMORY=201326592" futhark wasm prefix_sum.fut -o prefix_sum_wasm_seq
+EMCFLAGS="-s INITIAL_MEMORY=$MEMORY" futhark wasm prefix_sum.fut -o prefix_sum_wasm_seq
 
 echo "compiling multicore"
 futhark multicore prefix_sum.fut -o prefix_sum_mc
