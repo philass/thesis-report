2ab7890671c40c99fbfef69c3c5a87d8c8e71aa9
Philip Lassen <philiplassen+git@gmail.com>
2021-06-11 01:16:12 -0700

    Minor script fixups

--- fubench/bench.sh
+++ fubench/bench.sh
@@ -1,21 +1,24 @@
 PROG=${1%.fut}
 BASE=${PROG##*/}
 INPUT=$2
+INPUT_BASE=${INPUT%.*}
 
 bash compile.sh $PROG.fut
 
 echo "c"
-cat $2 | ./${BASE}_seq -t /dev/stdout
+cat $2 | ./${BASE}_c -t ${BASE}_c_${INPUT_BASE}
 echo "multicore"
-cat $2 | ./${BASE}_mc -t /dev/stdout
+cat $2 | ./${BASE}_mc -t ${BASE}_mc_${INPUT_BASE}
 echo "wasm"
-cat $2 | node --experimental-wasm-simd ./${BASE}_wasm_seq -t /dev/stdout
+cat $2 | node --experimental-wasm-simd ./${BASE}_wc -t /dev/stderr 2> ${BASE}_wc_${INPUT_BASE}
 echo "wasm-multicore"
-cat $2 | timeout 1 node --experimental-wasm-simd --experimental-wasm-threads ./${BASE}_wasm_mc -t /dev/stdout
+cat $2 | timeout 8 node --experimental-wasm-simd --experimental-wasm-threads ./${BASE}_wmc -t /dev/stderr 2> ${BASE}_wmc_${INPUT_BASE}
 
 bash clean.sh $PROG.fut
 
-
+echo "c mc wc wmc"
+cat ${BASE}_{,m,w,wm}c_${INPUT_BASE}
+ 
 
 # If i want to extract the compiled inputs I can use Soren's nifty one liner
 # One line from here should than be taken from here and be pasted into data.in
--- fubench/clean.sh
+++ fubench/clean.sh
@@ -1,7 +1,7 @@
 PROG=${1%.fut}
 BASE=${PROG##*/}
 
-rm ${BASE}{_wasm_mc,_wasm_seq,_seq,_mc}
-rm ${BASE}{_wasm_mc,_wasm_seq,_seq,_mc}.c
-rm ${BASE}{_wasm_mc,_wasm_seq}.wasm
-rm ${BASE}_wasm_mc.worker.js
+rm ${BASE}{_wmc,_wc,_c,_mc}
+rm ${BASE}{_wmc,_wc,_c,_mc}.c
+rm ${BASE}{_wmc,_wc}.wasm
+rm ${BASE}_wmc.worker.js
