7afea345b6894a7a2c5859c14e69564054543e38
Philip Lassen <philiplassen+git@gmail.com>
2021-06-14 03:31:32 -0700

    Add libnames.js

---
+++ code/compiler/c-backend/libnames.js
@@ -0,0 +1,11 @@
+class FutharkContext {
+  constructor()
+  get_entry_points()
+  to_futhark_i32_1d_arr(arr, dim0)
+  to_futhark_i32_2d_arr(arr, dim0, dim1)
+  from_futhark_shape_i32_1d_arr(fut_arr)
+  from_futhark_shape_i32_2d_arr(fut_arr)
+  from_futhark_values_i32_1d_arr(fut_arr)
+  from_futhark_values_i32_2d_arr(fut_arr)
+  col_sum(in0)
+}
